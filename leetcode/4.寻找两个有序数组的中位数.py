40e4bf2fefe43572faa174145eedef85b87241b3
morsoli <griezmannlimore@gmail.com>
2019-03-26 21:28:23 +0800

    ...

---
+++ leetcode/4.寻找两个有序数组的中位数.py
@@ -0,0 +1,75 @@
+#
+# @lc app=leetcode.cn id=4 lang=python3
+#
+# [4] 寻找两个有序数组的中位数
+#
+# https://leetcode-cn.com/problems/median-of-two-sorted-arrays/description/
+#
+# algorithms
+# Hard (34.03%)
+# Total Accepted:    42.3K
+# Total Submissions: 124.4K
+# Testcase Example:  '[1,3]\n[2]'
+#
+# 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+# 
+# 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+# 
+# 你可以假设 nums1 和 nums2 不会同时为空。
+# 
+# 示例 1:
+# 
+# nums1 = [1, 3]
+# nums2 = [2]
+# 
+# 则中位数是 2.0
+# 
+# 
+# 示例 2:
+# 
+# nums1 = [1, 2]
+# nums2 = [3, 4]
+# 
+# 则中位数是 (2 + 3)/2 = 2.5
+# 
+# 
+#
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            nums1, nums2, m, n = nums2, nums1, n, m
+        if n == 0:
+            raise ValueError
+
+        imin, imnums1x, half_len = 0, m, (m + n + 1) / 2
+        while imin <= imax:
+            i = (imin + imax) / 2
+            j = half_len - i
+            if i < m and nums2[j-1] > nums1[i]:
+                # i is too small, must increase it
+                imin = i + 1
+            elif i > 0 and nums1[i-1] > nums2[j]:
+                # i is too big, must decrease it
+                imax = i - 1
+            else:
+                # i is perfect
+
+                if i == 0: 
+                    max_of_left = nums2[j-1]
+                elif j == 0: 
+                    max_of_left = nums1[i-1]
+                else: 
+                    max_of_left = max(nums1[i-1], nums2[j-1])
+
+                if (m + n) % 2 == 1:
+                    return max_of_left
+
+                if i == m: 
+                    min_of_right = nums2[j]
+                elif j == n: 
+                    min_of_right = nums1[i]
+                else:
+                     min_of_right = min(nums1[i], nums2[j])
+
+                return (max_of_left + min_of_right) / 2.0
