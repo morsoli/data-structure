7ffd3d5319329f6291df6dc9675002e6f7f5cb7a
MorsoLi <2morsoli@gmail.com>
2019-07-19 09:52:31 +0800

    test

--- 精选 TOP 面试题/5.最长回文子串.py
+++ 精选 TOP 面试题/5.最长回文子串.py
@@ -8,12 +8,14 @@ class Solution:
         start=0
         maxstr=0
         for i in range(len(s)):
+            # 头和尾相同时，最长回文子串是去头去尾之后的部分的最长回文子串加上头和尾
             if i-maxstr>=1 and s[i-maxstr-1:i+1]==s[i-maxstr-1:i+1][::-1]:
-                start = i-maxstr-1
+                start=i-maxstr-1
                 maxstr+=2
                 continue
-            if  i-maxstr>=0 and s[i-maxstr:i+1]==s[i-maxstr:i+1][::-1]:
-                start = i-maxstr
+            # 头和尾不同，最长回文子串是去头的部分的最长回文子串和去尾的部分的最长回文子串中的较长的那一个。
+            if s[i-maxstr:i+1]==s[i-maxstr:i+1][::-1]:
+                start=i-maxstr
                 maxstr+=1
         return s[start:start+maxstr]
        
